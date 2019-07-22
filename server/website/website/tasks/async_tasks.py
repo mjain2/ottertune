db57695422cb55b161383b75056b561961a1ff80
arifiorino <arifiorino@gmail.com>
2019-07-21 20:00:17 -0400

    Fixed syntax errors

--- server/website/website/tasks/async_tasks.py
+++ server/website/website/tasks/async_tasks.py
@@ -372,21 +372,18 @@ def configuration_recommendation(target_data):
             col_max = X_scaled[:, i].max()
             for knob in session_knobs:
                 if X_columnlabels[i] == knob.name:
-                    col_min = knob.minval
-                    col_max = knob.maxval
-'''
-            if X_columnlabels[i] in knobs_mem_catalog:
-                X_mem[0][i] = mem_max * 1024 * 1024 * 1024  # mem_max GB
-                col_max = min(col_max, X_scaler.transform(X_mem)[0][i])
-'''
-
-            # Set min value to the default value
-            # FIXME: support multiple methods can be selected by users
-            col_min = X_default_scaled[i]
-
+                    col_min = X_scaler.transform(knob.minval)[0][i]
+                    col_max = X_scaler.transform(knob.maxval)[0][i]
+#            if X_columnlabels[i] in knobs_mem_catalog:
+#                X_mem[0][i] = mem_max * 1024 * 1024 * 1024  # mem_max GB
+#                col_max = min(col_max, X_scaler.transform(X_mem)[0][i])
+#            # Set min value to the default value
+#            # FIXME: support multiple methods can be selected by users
+#            col_min = X_default_scaled[i]
         X_min[i] = col_min
         X_max[i] = col_max
         X_samples[:, i] = np.random.rand(num_samples) * (col_max - col_min) + col_min
+
 
     # Maximize the throughput, moreisbetter
     # Use gradient descent to minimize -throughput
